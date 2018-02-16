7ce8bcf9d7e1d8938e829453c5c3c2856305988a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-16 19:01:17 +0100

    ok

---
+++ app/03-repeat/main.js
@@ -0,0 +1,15 @@
+// Init 
+
+var array = [3, 5, 8, 2];
+
+var listElt = document.querySelector('#list');
+var liElt = listElt.querySelector('li');
+
+listElt.innerHTML = '';
+array.forEach((i) => {
+    var node = liElt.cloneNode(true);
+    node.querySelector('span').innerHTML = i;
+    listElt.appendChild(node);
+});
+
+// aie aie aie it is a lot of code for something simple...
