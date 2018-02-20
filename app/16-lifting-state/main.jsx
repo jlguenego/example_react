f28e9a297b319948a9c1fdfe894992b234645ac6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-20 17:36:54 +0100

    stars working

--- app/16-lifting-state/main.jsx
+++ app/16-lifting-state/main.jsx
@@ -5,54 +5,93 @@ import '../css/style.scss';
 
 class StarInput extends React.Component {
     constructor(props) {
+        console.log('StarInput constructor');
         super(props);
         this.state = {
-            note: 3
+            note: 3,
         };
-        if (isNaN(+props.note)) {
+        this.componentWillReceiveProps(props);
+    }
+
+    componentWillReceiveProps(props) {
+        console.log('new props', props)
+        if (!'note' in props) {
+            return;
+        }
+        if (isNaN(+props.note) ||
+            +props.note > 5 ||
+            +props.note < 0
+        ) {
             this.state.note = undefined;
-        } else if (props.note > 5) {
-            this.state.note = undefined;
-        } else if (props.note < 0) {
-            this.state.note = undefined;
-        } else if (props.note) {
-            this.state.note = props.note;
+        } else {
+            this.state.note = +props.note;
+        }
+        console.log('this.state', this.state);
+    }
+
+    onClick(n, e) {
+        e.preventDefault();
+        console.log('onClick', n, e, this);
+        const state = {
+            note: n,
+        };
+        this.setState(state);
+        if (this.props.onChange) {
+            this.props.onChange(n);
         }
     }
 
-    // onSubmit(event) {
-    //     console.log('onSubmit', this);
-    //     // in order to not let the browser go to another page.
-    //     event.preventDefault();
-    // }
-
-    // onChange(event) {
-    //     console.log('onChange', event.target);
-    //     const state = {};
-    //     state[event.target.name] = event.target.value;
-    //     this.setState(state);
-    // }
-
     render() {
+        console.log('StarInput render', this.state);
         const starList = [1, 2, 3, 4, 5].map(n => {
-            if (this.state.note === undefined) {
-                return <img key={n} src="img/gray_star.png" />
+            let src = 'img/gray_star.png';
+            if (!isNaN(this.state.note)) {
+                if (n <= this.state.note) {
+                    src = 'img/yellow_star.png';
+
+                } else {
+                    src = 'img/white_star.png';
+                }
             }
-            return (n <= this.state.note) ? <img key={n} src="img/yellow_star.png" /> : <img key={n} src="img/white_star.png" />;
+            return <img onClick={this.onClick.bind(this, n)} key={n} src={src} style={{ cursor: 'pointer' }} />;
         });
 
         return (
             <div>
                 {starList}
+                {JSON.stringify(this.state)}
             </div>
         )
     }
 }
 
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            appNote: 4
+        };
+    }
+    onChange(n) {
+        console.log('onChange', n, this);
+        this.setState({ appNote: +n });
+    }
+
+    render() {
+        console.log('app render', this.state);
+        const result = (
+            <div>
+                <StarInput />
+                <StarInput note={this.state.appNote} onChange={this.onChange.bind(this)} />
+                <StarInput note={this.state.appNote} onChange={this.onChange.bind(this)} />
+                {JSON.stringify(this.state)}
+            </div>
+        );
+        console.log('result', result);
+        return result;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<div>
-        <StarInput />
-        <StarInput note="4" />
-        <StarInput note="6" />
-    </div>, document.getElementById('root'));
+    ReactDOM.render(<App />, document.getElementById('root'));
 });
