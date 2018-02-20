4fcca73b23bd1774974333bf23a67215f2e2cc8a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-20 15:02:07 +0100

    observable

--- app/01-html-js-css/main.js
+++ app/01-html-js-css/main.js
@@ -1,2 +1,38 @@
-console.log('Hello world');
-document.querySelector('#title').innerHTML = 'Hello world!';
+(function () {
+    'use strict';
+
+    // Hello world to the console
+    console.log('Hello world');
+    // Talk to the DOM.
+    // Question ? What if the main.js is loaded before the DOM ?
+    document.querySelector('#title').innerHTML = 'Hello world!';
+
+    // Observables since ES5.1
+    const hello = {
+        get world() {
+            console.log('try to access to the world...');
+            return this.$$hiddenWorld
+        },
+
+        set world(value) {
+            console.log('try to set something to the world...', value);
+            this.hiddenWorld = value;
+            return this.$$hiddenWorld
+        }
+    }
+
+    Object.defineProperty(hello, '$$hiddenWorld', {
+        value: 'coucou',
+        configurable: false,
+        writable: true,
+        enumerable: false,
+    });
+    console.log('hello', hello);
+
+    console.log('hello.world', hello.world);
+    hello.world = 'foo';
+
+})();
+
+
+// some element of Javascript.
