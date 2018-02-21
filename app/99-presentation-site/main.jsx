f40968f9baa58875ec6d44c36987e03d6a85309b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-21 10:17:34 +0100

    site pres

---
+++ app/99-presentation-site/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css';
+import './css/reset.scss';
+import './css/layout.scss';
+import './css/theme.scss';
+
+import { Header } from './jlg-layout/Header';
+import { Body } from './jlg-layout/Body';
+import { Footer } from './jlg-layout/Footer';
+
+
+
+function Main() {
+    return (
+        <React.Fragment>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </React.Fragment>
+    );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<Main />, document.body);
+});
