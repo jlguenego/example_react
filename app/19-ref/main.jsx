8ebf240154d7ad6cffbc158bbe3fc852c9d77d2f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-22 22:20:22 +0100

    ref

---
+++ app/19-ref/main.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../css/style.scss';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: false
+        };
+    }
+
+    switch() {
+        this.setState(prevState => {
+            return { on: !prevState.on };
+        });
+    }
+
+    onRef(...args) {
+        console.log('onRef', args, this);
+    }
+
+    render() {
+        console.log('render app');
+        return (
+            <div>
+                <span ref={this.onRef.bind(this)} />
+                <button onClick={this.switch.bind(this)}>{this.state.on ? 'Hide' : 'Show'}</button>
+                {this.state.on ? <Hello ref={this.onRef} name="JL" /> : ''}
+            </div>
+        );
+    }
+}
+
+class Hello extends React.Component {
+    render() {
+        return <span>Hello {this.props.name}!</span>;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+});
