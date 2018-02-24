e31eb4133d835fab561ff872cc8b2054fa376b1b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-24 20:31:57 +0100

    ok

--- app/22-life-cycle/main.jsx
+++ app/22-life-cycle/main.jsx
@@ -15,12 +15,17 @@ function sleep(time) {
 
 class App extends React.Component {
 
+    componentWillMount() {
+        console.log('componentWillMount', this.props.id);
+    }
+
     componentDidMount() {
         console.log('componentDidMount', this.props.id);
     }
     componentWillUnmount() {
         console.log('componentWillUnmount', this.props.id);
     }
+    
     render() {
         console.log('render', this.props.id);
         return (<div >
@@ -31,7 +36,14 @@ class App extends React.Component {
 
 document.addEventListener('DOMContentLoaded', async () => {
     console.log('DOM loaded');
-    let app = <div><App id="1"/><App id="2"/></div>;
+    let app = <div>
+        <App id="1"/>
+        <App id="2"/>
+        <App id="3"/>
+        <App id="4"/>
+        <App id="5"/>
+        <App id="6"/>
+        </div>;
     await sleep(2000);
     ReactDOM.render(app, document.getElementById('root'));
     await sleep(2000);
