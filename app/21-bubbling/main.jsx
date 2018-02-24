c226e87c09de6d72976b8ce0e750b55bc3eb1cc2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-24 19:27:52 +0100

    ok

---
+++ app/21-bubbling/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../css/style.scss';
+
+function Card(props) {
+    return (
+        <div>
+            <img style={{ width: '100px' }} src="../img/react.svg" />
+            <figcaption>{props.label}</figcaption>
+        </div>
+    );
+}
+
+function click(e) {
+    console.log('click!', e.target, e.currentTarget.children);
+    const index = [].indexOf.call(e.currentTarget.children, e.target.closest('div'));
+    console.log('index', index);
+}
+
+const App = () => <div onClick={click}>
+    <Card label="React 1" />
+    <Card label="React 2" />
+</div>;
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+});
