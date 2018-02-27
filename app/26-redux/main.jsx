03604f99add29bf025097c37982f1521ff096b81
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-27 10:06:37 +0100

    redux working

--- app/26-redux/main.jsx
+++ app/26-redux/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 
 import '../css/style.scss';
 
@@ -31,18 +32,19 @@ const initialState = {
 };
 
 // 4) define the reducers
-function incrementReducer(previousState, action) {
+function incrementReducer(previousState) {
     const newState = { ...previousState };
-    return newState.number++;
+    newState.number++;
+    return newState;
 }
 
 function addReducer(previousState, action) {
     const newState = { ...previousState };
-    return newState.number += action.n;
+    newState.number += action.n;
+    return newState;
 }
 
 function reducer(previousState = initialState, action) {
-
     switch (action.type) {
         case actionType.INCREMENT:
             return incrementReducer(previousState, action);
@@ -53,18 +55,63 @@ function reducer(previousState = initialState, action) {
     }
 }
 
+// 5) create the store
+let store = createStore(reducer, initialState);
 
+// Log the initial state
+console.log('initial state', store.getState());
 
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log('current state', store.getState())
+);
 
+store.dispatch(increment());
+store.dispatch(add(2));
+
+// stop listening the actions.
+unsubscribe();
+
+// 6) use react-redux to create a container component.
+const mapStateToProps = state => ({ number: state.number });
+const mapDispatchToProps = dispatch => ({
+    increment: () => dispatch(increment()),
+    add: (n) => dispatch(add(n)),
+});
 
 function App(props) {
+    console.log('props', props);
+    let input;
+    let value = -1;
+    const ref = n => {
+        input = n;
+        if (input) {
+            input.value = value;
+        }
+    };
     return (<div>
+        <h2>Value of counter: {props.number}</h2>
         <button onClick={props.increment}>Increment</button>
-        {props.number}
-    </div>);
+        <button onClick={props.add.bind(undefined, 2)}>Add 2</button>
+        <br />
+        <input ref={ref} type="number" />
+        <br />
+        <button onClick={() => { props.add(+input.value); }}>Add input</button>
 
+    </div>);
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-    ReactDOM.render(<Container />, document.getElementById('root'));
+
+const Container = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<Provider store={store}>
+        <Container />
+    </Provider>, document.getElementById('root'));
 });
