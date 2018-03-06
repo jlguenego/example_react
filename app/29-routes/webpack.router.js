4989847fabbd3cbcc204411638497531d235c79e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-06 12:47:23 +0100

    ok

--- app/29-routes/webpack.router.js
+++ app/29-routes/webpack.router.js
@@ -4,8 +4,8 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 let webpackConfig = require('./webpack.config.js');
-webpackConfig = {...webpackConfig};
-webpackConfig.output = {...webpackConfig.output};
+webpackConfig = { ...webpackConfig };
+webpackConfig.output = { ...webpackConfig.output };
 webpackConfig.output.publicPath = 'http://localhost:8000/app/29-routes/wpk/';
 
 webpackConfig.entry = {
@@ -21,12 +21,17 @@ webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 const compiler = webpack(webpackConfig);
 // eslint-disable-next-line
 const router = express.Router();
-router.use(webpackDevMiddleware(compiler, {
-    noInfo: true, 
-    publicPath: '/wpk/',
+router.use('/wpk/', webpackDevMiddleware(compiler, {
+	noInfo: true,
 }));
 router.use(webpackHotMiddleware(compiler, {
-    // log: true,
-    path: '/__what',
+	// log: true,
+	path: '/__what',
 }));
+
+
+// URL rewriting
+router.use((req, res, next) => {
+	res.sendFile('./index.html', { root: __dirname });
+});
 module.exports = router;
