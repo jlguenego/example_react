7e13418b9a3f659f9381a10582b3de7df2d08435
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-03-06 17:47:21 +0100

    ok

--- app/31-server-routes/src/Html.jsx
+++ app/31-server-routes/src/Html.jsx
@@ -8,7 +8,7 @@ const Head = () => (
     <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Isomorphic App</title>
+        <title>Server App</title>
     </head>
 );
 
