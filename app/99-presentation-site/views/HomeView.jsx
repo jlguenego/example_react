1c32fdf85ab15f3f6607f18c8039d36fb1a5438c
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-03-08 11:22:33 +0100

    ok

--- app/99-presentation-site/views/HomeView.jsx
+++ app/99-presentation-site/views/HomeView.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const HomeView = () => <main>
-    <section className="jumbo">
-        <div>
-            <p className="tagline">Use React for your webapp !</p>
-            <p className="buttons">
-                <Link to="/examples" className="button primary">See our examples</Link>
-                <Link to="/services" className="button secondary">Check our services</Link>
-            </p>
-        </div>
-    </section>
-    <section className="social-media-share">
-        <button className="facebook"><i className="fa fa-facebook-square"></i> <b>Share on Facebook</b></button>
-    </section>
-</main>;
+const HomeView = () =>
+    <main>
+        <section className="jumbo">
+            <div>
+                <p className="tagline">Use React for your webapp !</p>
+                <p className="buttons">
+                    <Link to="/examples" className="button primary">See our examples</Link>
+                    <Link to="/services" className="button secondary">Check our services</Link>
+                </p>
+            </div>
+        </section>
+        <section className="social-media-share">
+            <button className="facebook">
+                <i className="fa fa-facebook-square"></i>
+                <b>Share on Facebook</b>
+            </button>
+        </section>
+    </main>;
 
 export default HomeView;
