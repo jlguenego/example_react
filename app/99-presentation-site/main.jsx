fb6e2d6c5224d11c881579864ee52a8ba6bc7896
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-09 10:26:43 +0100

    warning stuff

--- app/99-presentation-site/main.jsx
+++ app/99-presentation-site/main.jsx
@@ -23,6 +23,7 @@ function isMobile() {
 function render() {
     console.log('render', isMobile());
     // Note: here you may have a warning because the isMobile can be true.
+    // Todo: rewrite this with react-responsive. (>2500 github stars)
     ReactDOM.hydrate(<Layout basename={basename} isMobile={isMobile()}/>, document.getElementById('root'));
 }
 
