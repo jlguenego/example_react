c6d9987cd31ef022dcfb0d5aef52d44de9f0827d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-27 19:17:59 +0100

    prettier

--- app/08-component/main.jsx
+++ app/08-component/main.jsx
@@ -1,32 +1,31 @@
-
 // React functional component.
 function Hello1(props) {
-    return <div className="hello">Hello {props.name}</div>;
+  return <div className="hello">Hello {props.name}</div>;
 }
 
 // React functional component (arrow notation).
-const Hello1a = (props) => <div className="hello">Hello hello {props.name}</div>;
+const Hello1a = props => <div className="hello">Hello hello {props.name}</div>;
 
 ReactDOM.render(<Hello1 name="Jean-Louis" />, document.getElementById('root'));
 
 // React class component.
 class Hello2 extends React.Component {
-
-    render() {
-        return <div className="hello">Hello dear {this.props.name}</div>;
-    }
+  render() {
+    return <div className="hello">Hello dear {this.props.name}</div>;
+  }
 }
 
 ReactDOM.render(<Hello2 name="Dany" />, document.getElementById('other-root'));
 
-
 // A component can call other components.
 function MyComponent() {
-    return (<div>
-        <Hello1 name="Alice" />
-        <Hello1a name="Anna" />
-        <Hello2 name="Bob" />
-    </div>);
+  return (
+    <div>
+      <Hello1 name="Alice" />
+      <Hello1a name="Anna" />
+      <Hello2 name="Bob" />
+    </div>
+  );
 }
 
 ReactDOM.render(<MyComponent />, document.getElementById('root3'));
