efa2c3ad9cb074ddb14bd9cc040163d0267c5782
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-27 19:15:53 +0100

    upgraded react

--- app/07-react-element/main.jsx
+++ app/07-react-element/main.jsx
@@ -1,31 +1,29 @@
 function time() {
-    return new Date().toLocaleTimeString();
+  return new Date().toLocaleTimeString();
 }
-
 
 // Only the diff will be modified in the DOM.
 // The DOM will not be naively rewritten.
 // This is the advantage of Virtual DOM.
 setInterval(() => {
-    console.log('tick');
-    const element = (
-        <div>
-            <h2>Date</h2>
-            <div>Time : {time()}</div>
-        </div>
-    );
+  console.log('tick');
+  const element = (
+    <div>
+      <h2>Date</h2>
+      <div>Time : {time()}</div>
+    </div>
+  );
 
-    ReactDOM.render(element, document.querySelector('#root'));
+  ReactDOM.render(element, document.querySelector('#root'));
 }, 1000);
 
 setInterval(() => {
-    console.log('tick vanilla');
+  console.log('tick vanilla');
 
-    document.querySelector('#root-vanilla').innerHTML = `
+  document.querySelector('#root-vanilla').innerHTML = `
 <div>  
     <h2>Date</h2>
     <div>Time : ${time()}</div>
 </div>
     `;
 }, 1000);
-
