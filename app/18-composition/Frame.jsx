48b7df1805e283cd9ed197cb2caf326892711c9d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-28 19:09:38 +0100

    update 18

--- app/18-composition/Frame.jsx
+++ app/18-composition/Frame.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 // Look at the special props.children property.
 
 export function Frame(props) {
-    return (
-        <div style={{ border: '1px solid black' }}>
-            {props.children}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        border: '0.1em solid red',
+        margin: '0.5em',
+      }}
+    >
+      {props.children}
+    </div>
+  );
 }
-
