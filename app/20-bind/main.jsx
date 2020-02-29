eba03458f6d2713c9dd643959e297beee3abc04f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-29 12:44:34 +0100

    update ex 20

--- app/20-bind/main.jsx
+++ app/20-bind/main.jsx
@@ -4,32 +4,31 @@ import ReactDOM from 'react-dom';
 import '../css/style.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: '',
-        };
-    }
+  handleClick(...args) {
+    console.log('handleClick', args, this);
+    this.setState({ message: 'Hello !!!' });
+  }
 
-    handleClick(...args) {
-        console.log('handleClick', args, this);
-        this.setState({ message: 'Hello !!!' });
-    }
-
-    render() {
-        // <> is <React.Fragment> in short syntax.
-        // :: is bind syntax (TC39 bind operator)
-        // On Feb 2018, VSCode is not ready for checking this new JS syntax :(
-        return (
-            <>
-                <button onClick={::this.handleClick}>Click me!</button>
-                <span>{this.state.message}</span>
-            </>
-        );
-    }
+  render() {
+    // <> is <React.Fragment> in short syntax.
+    // :: is bind syntax (TC39 bind operator)
+    // On Feb 2018, VSCode is not ready for checking this new JS syntax :(
+    return (
+      <>
+        <button onClick={::this.handleClick}>Click me!</button>
+        <span>{this.state.message}</span>
+      </>
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById('root'));
 });
