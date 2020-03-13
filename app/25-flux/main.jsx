9ad7910d01278c0eb44bbfd2c411361a5182b93f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-13 15:23:58 +0100

    no more security issue

--- app/25-flux/main.jsx
+++ app/25-flux/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -9,66 +10,69 @@ import '../css/style.scss';
 const dispatcher = new Dispatcher();
 
 class MyStore extends ReduceStore {
-    constructor() {
-        super(dispatcher);
+  constructor() {
+    super(dispatcher);
+  }
+
+  getInitialState() {
+    return { number: 10 };
+  }
+
+  reduce(prevState, action) {
+    const t = action.type;
+    if (t === 'increment') {
+      const state = { ...prevState };
+      state.number++;
+      return state;
+    }
+    if (t === 'add') {
+      return { number: prevState.number + action.n };
     }
 
-    getInitialState() {
-        return { number: 10 };
-    }
-
-    reduce(prevState, action) {
-        let state;
-        switch (action.type) {
-            case 'increment':
-                // it is required to send a brand new state. Not the existing one.
-                // we call that an immutable object.
-                state = { ...prevState };
-                state.number++;
-                return state;
-            case 'add':
-                return { number: prevState.number + action.n };
-
-            default:
-                return prevState;
-        }
-    }
+    return prevState;
+  }
 }
 
 const myStore = new MyStore();
 
 function App(props) {
-    return (<div>
-        <button onClick={props.onIncrement}>Increment</button>
-        <button onClick={props.onAdd.bind(props, 2)}>Add 2</button>
-        {props.counter.number}
-    </div>);
+  return (
+    <div>
+      <button onClick={props.onIncrement}>Increment</button>
+      <button onClick={props.onAdd.bind(props, 2)}>Add 2</button>
+      {props.counter.number}
+    </div>
+  );
 }
 
-const RootContainer = Container.createFunctional(App, () => [myStore], () => {
+const RootContainer = Container.createFunctional(
+  App,
+  () => [myStore],
+  () => {
     return {
-        counter: myStore.getState(),
-        onIncrement: () => {
-            dispatcher.dispatch({
-                type: 'increment',
-            });
-        },
-        onAdd: (n) => {
-            dispatcher.dispatch({
-                type: 'add',
-                n
-            });
-        },
+      counter: myStore.getState(),
+      onIncrement: () => {
+        dispatcher.dispatch({
+          type: 'increment',
+        });
+      },
+      onAdd: n => {
+        dispatcher.dispatch({
+          type: 'add',
+          n,
+        });
+      },
     };
-});
+  }
+);
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<RootContainer />, document.getElementById('root'));
+  ReactDOM.render(<RootContainer />, document.getElementById('root'));
 });
 
 setInterval(() => {
-    dispatcher.dispatch({
-        type: 'add',
-        n: -1
-    });
-}, 5000);
+  dispatcher.dispatch({
+    type: 'add',
+    n: -1,
+  });
+}, 2000);
